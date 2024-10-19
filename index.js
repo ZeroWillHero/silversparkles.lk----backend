ac3733ac740fbf8e8e93d85256ef28b603d284c1
ZeroWilHero <ict21018@std.uwu.ac.lk>
2024-10-19 15:54:39 +0530

    cors updated

--- index.js
+++ index.js
@@ -21,10 +21,14 @@ const app = express();
 var corsOptions = {
     origin: 'https://silverhttps://silversparkles-admin.netlify.app/',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+}
 
 
-app.use(cors());
+// app.use(cors());
+
+app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for only example.com.'})
+  })
 
 // handle cors error
 app.use('/uploads', express.static('uploads'));
