29fd974dc554872dda5ff2157536042c4bece314
ZeroWilHero <ict21018@std.uwu.ac.lk>
2024-11-24 09:55:40 +0530

    width, length and color and length fields are set as not required fields

--- module/product/controllers/addProducts.js
+++ module/product/controllers/addProducts.js
@@ -1,51 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const addProducts = async (req, res) => {
-//   const Product = mongoose.model('product');
-//  // one product add 
-//   // try {
-//   //   const newProduct = new Product({
-//   //     title: req.body.title,
-//   //     category: req.body.category,
-//   //     price: req.body.price,
-//   //     description: req.body.description,
-//   //     stock: req.body.stock,
-//   //     metal: req.body.metal,
-//   //     weight: req.body.weight,
-//   //     length: req.body.length,
-//   //     width: req.body.width,
-//   //     ring_size: req.body.ring_size,
-//   //     color: req.body.color,
-//   //     stone: req.body.stone,
-//   //     images: req.body.images,
-//   //     gender: req.body.gender,
-//   //     iced_product: req.body.iced_product,
-//   //     style: req.body.style,
-//   //   });
-//   //   await newProduct.save();
-//   //   res.status(201).json ({ status: "success", product: newProduct, message: "Product added successfully" });
-//   // } catch (error) {
-//   //   res.status(409).json({ status: "unsuccess", message: error.message });
-//   // }
-
-
-//   // bulk products add 
-//   try {
-//     const products = req.body; // assuming that you're sending an array of products
-//     const newProducts = products.map(product => new Product(product));
-//     const result = await Product.insertMany(newProducts);
-//     res.status(201).json({status : "success", message : "Products added successfully", data: result});
-// } catch (error) {
-//     res.status(409).json({ status:"unsuccess", message: error.message });  
-// }
-// };
-
-// module.exports = addProducts;
-
-
-
-// -----------------------------sequelize Mysql------------------------------------------------
-
 const fs = require('fs');
 const path = require('path');
 const db = require("../../../model/mysql/index");
@@ -54,7 +6,6 @@ const cloudinary = require("../../../manager/cloudnary");
 // Mysql DB model call
 const Product = db.products;
 const multer = require('multer');
-
 
 const addProducts = async (req, res) => {
     const {
@@ -71,50 +22,32 @@ const addProducts = async (req, res) => {
         iced_product,
         style,
         ring_size,
+        length,
+        color
     } = req.body;
 
-    console.log("length  ", length)
-    console.log("color  ", color)
-
     try {
-        // const images = req.files.map(file => {
-        //     return {
-        //         url: '/uploads/' + file.filename // Store the file path in the database//
-        //     };
-        // });
-
-        let color1 = [];
         let length1 = [];
-        let ring_size = '';
+        let color1 = [];
 
-        if (req.body.ring_size){
-            ring_size = req.body.ring_size;
-        }
-
-
-        if (req.body.length) {
+        if (length) {
             length1 = length.map(len => {
                 return {
                     length: len
-                }
-            })
+                };
+            });
         }
 
-        if (req.body.color) {
+        if (color) {
             color1 = color.map(co => {
                 return {
                     color: co
-                }
-            })
-
+                };
+            });
         }
 
-        console.log("color", color1)
-
-        //console.log("length1", length1)
-
         // Check if all required fields are present
-        const requiredFields = ['title', 'category', 'price', 'description', 'stock',  'stone', 'gender', 'style','metal'];
+        const requiredFields = ['title', 'category', 'price', 'description', 'stock', 'stone', 'gender', 'style', 'metal'];
         const missingFields = requiredFields.filter(field => !req.body[field]);
 
         if (missingFields.length > 0) {
@@ -135,7 +68,6 @@ const addProducts = async (req, res) => {
             return res.status(400).json({ error: 'At least one image is required' });
         }
 
-
         const product = await Product.create({
             title,
             category,
@@ -144,10 +76,10 @@ const addProducts = async (req, res) => {
             stock,
             metal,
             weight,
-            length: JSON.stringify(length1),
-            width,
-            ring_size : ring_size,
-            color: JSON.stringify(color1),
+            length: length1.length ? JSON.stringify(length1) : null,
+            width: width || null,
+            ring_size: ring_size || null,
+            color: color1.length ? JSON.stringify(color1) : null,
             stone,
             images: JSON.stringify(images), // Store image paths as JSON string in the database
             gender,
@@ -163,4 +95,3 @@ const addProducts = async (req, res) => {
 };
 
 module.exports = addProducts;
-
